1499bf0ded6409ea84eb6d1a9cab9ff1241778db
Spencer Meredith <Smeredith1119@gmail.com>
2019-04-17 16:12:36 -0700

    working on file

--- api/file.js
+++ api/file.js
@@ -1,6 +1,25 @@
 // Imports
 
 
+const server = express();
+const cors = require("cors");
 
+const express = require("express");
+
+const helmet = require("helmet");
+
+
+
+
+
+
+
+
+
+
+
+});
+
+module.exports = server;
 
 //
--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 
 const server = require('api/file.js');
 
+
+const file = 
+
+require('dotenv').config()
+require('./api/file');
+
 // Ports 
+const port = process.env.PORT || 7000
 
-const port = 7000;
+file.listen(port, ()=> {
 
 
+});
 
-server.listen(port, () => {
+
     
-}); 
